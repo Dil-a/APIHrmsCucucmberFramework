cd2d54c67c03d870adbf9d4c41b81f598186ec8c
kenzhebek zholdoshov <dilbarab5@gmail.com>
2020-02-29 13:07:06 -0500

    Api cucumber framework

--- src/test/java/com/hrms/API/steps/practice/POSTCreateEmployeeAPI.java
+++ src/test/java/com/hrms/API/steps/practice/POSTCreateEmployeeAPI.java
@@ -1,8 +1,10 @@
 package com.hrms.API.steps.practice;
 
 import cucumber.api.java.en.Given;
+import static org.hamcrest.Matchers.equalTo;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.*;
@@ -28,19 +30,22 @@ public class POSTCreateEmployeeAPI {
 	@When("User retrieves response for createEmployee API")
 	public void user_retrieves_response_for_createEmployee_API() {
 
-		request.when().post(APIConstants.CREATE_EMPLOYEE_URI);
+		response=request.when().post(APIConstants.CREATE_EMPLOYEE_URI);
+		response.prettyPrint();
 	}
 
 	@Then("status code is {int} for createEmployeeAPI")
-	public void status_code_is_for_createEmployeeAPI(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+	public void status_code_is_for_createEmployeeAPI(int int1) {
+		response.then().assertThat().statusCode(int1);
 	}
 
 	@Then("user validates employee was created")
 	public void user_validates_employee_was_created() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+			response.then().body("Message", equalTo("Entry created"));
+			response.then().body("Employee[0]emp_firstname", equalTo(""));
+			JsonPath jsonPathEvaluator=response.jsonPath();
+			String employee_id=jsonPathEvaluator.get("Employee[0].employee_id");
+			System.out.println(employee_id);
 	}
 
 }
